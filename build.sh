e771d772c13d257ba6ca398e18af92ff44a553bf
Brandon Lehmann <brandonlehmann@gmail.com>
2019-01-08 18:40:00 -0500

    Options are good, right?

--- build.sh
+++ build.sh
@@ -5,7 +5,9 @@
 if [[ -z "${EMSDK}" ]]; then
   echo "Installing emscripten..."
   echo ""
-  git clone https://github.com/juj/emsdk.git
+  if [[ ! -e ./emsdk ]]; then
+    git clone https://github.com/juj/emsdk.git
+  fi
   cd emsdk && git pull
   ./emsdk install latest && ./emsdk activate latest
   source ./emsdk_env.sh
@@ -31,7 +33,11 @@ echo " Done"
 # Compile the actual file
 echo ""
 echo -n "Transpiling Project from C++ to Javascript... "
-emcc -s WASM=0 --bind -std=c++17 c.bc cpp.bc ../src/turtlecoin-crypto.cpp -o turtlecoin-crypto.js
+emcc -s WASM=0 --bind -std=c++17 c.bc cpp.bc ../src/turtlecoin-crypto.cpp -o "turtlecoin-crypto.js"
+emcc -s WASM=0 -s ENVIRONMENT=node --bind -std=c++17 c.bc cpp.bc ../src/turtlecoin-crypto.cpp -o "turtlecoin-crypto-node.js"
+emcc -s WASM=0 -s ENVIRONMENT=web --bind -std=c++17 c.bc cpp.bc ../src/turtlecoin-crypto.cpp -o "turtlecoin-crypto-web.js"
+emcc -s WASM=0 -s ENVIRONMENT=worker --bind -std=c++17 c.bc cpp.bc ../src/turtlecoin-crypto.cpp -o "turtlecoin-crypto-worker.js"
+emcc -s WASM=0 -s ENVIRONMENT=shell --bind -std=c++17 c.bc cpp.bc ../src/turtlecoin-crypto.cpp -o "turtlecoin-crypto-shell.js"
 echo " Done"
 
 cd ..
